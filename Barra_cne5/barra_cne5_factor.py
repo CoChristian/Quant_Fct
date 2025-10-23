cb4cd7086a73dcdf7783989321eeb1f38e2e4c4a
Amadeous_wu <1242754584@qq.com>
2025-10-23 23:04:50 +0800

    add Barra

--- Barra_cne5/barra_cne5_factor.py
+++ Barra_cne5/barra_cne5_factor.py
@@ -166,8 +166,6 @@ class Beta(Calculation):
 
         grouped = df.grouby('code', group_keys=False)
 
-        def cal_WLS_beta_alpha(df):
-            df
 
         for stock_code, group in grouped:
             if group[group['TRADE_DT'] > '2010-01-01'].empty:
@@ -195,6 +193,215 @@ class Beta(Calculation):
                 df.loc[original_df_index, 'BETA'] = df.loc[original_df_index, 'BETA'].shift(1)
 
         df['SIGMA'] = df['STOCK_RETURN']- df['RF_RETURN'] - (df['ALPHA'] + df['BETA'] * df['MKT_RETURN'])
+
+        return df
+
+class Momentum(Calculation):
+    """
+    计算个股动量因子的类，继承自 Calculation 类
+    """
+
+    def RSTR(self, df: pd.DataFrame, raw: bool = False) -> pd.DataFrame:
+        """
+        计算个股的RSTR
+
+        :param df: 包含个股交易数据的 DataFrame，需包含以下列:
+                   - 'TRADE_DT': 交易日期 例: '2022-08-31'
+                   - 'S_INFO_WINDCODE': 个股代码 例: '000001.SZ'
+                   - 'S_DQ_CLOSE': 收盘价 例: 10.5
+                   - 'S_DQ_PRECLOSE': 前一日收盘价 例: 10.0
+        :param raw: 是否返回未经预处理的原始数据，默认为 False，即返回预处理后的数据
+        :return: 返回包含新增列 'RSTR' 的 DataFrame
+        """
+
+        # 检查无风险收益率数据并合并
+        if 'RF_RETURN' not in df.columns:
+            rf_df = GetData.risk_free()
+            df = df.merge(rf_df, on='TRADE_DT', how='left')
+
+        # 计算个股日收益率
+        df['STOCK_RETURN'] = df['S_DQ_CLOSE'] / df['S_DQ_PRECLOSE'] - 1
+        df['STOCK_RETURN'] = df['STOCK_RETURN'].astype('float')
+
+        # 生成指数加权权重
+        exp_weight = self._exp_weight(window=504, half_life=126)
+
+        # 按个股代码分组
+        grouped = df.groupby('S_INFO_WINDCODE')
+
+        for stock_code, group in grouped:
+            # 检查数据长度是否足够计算动量因子
+            if len(group) < 504 + 21:
+                print(f'Not enough data to calculate Momentum for {stock_code}')
+                df.loc[group.index, 'RSTR'] = np.nan
+
+            else:
+                # 计算超额对数收益率
+                group['EX_LG_RT'] = np.log(1 + group['STOCK_RETURN']) - np.log(1 + group['RF_RETURN'])
+
+                try:
+                    # 使用指数加权和滚动窗口计算 RSTR
+                    group['RSTR'] = group['EX_LG_RT'].shift(21).rolling(window=504).apply(
+                        lambda x: np.sum(x * exp_weight))
+
+                    # 将计算结果存入原始数据框
+                    df.loc[group.index, 'RSTR'] = group['RSTR']
+
+                except Exception as e:
+                    print(f'Error processing {stock_code}: {e}')
+                    df.loc[group.index, 'RSTR'] = np.nan
+
+        # 如果 raw 为 False，则进行预处理
+        if not raw:
+            df = self._preprocess(df, factor_column='RSTR')
+
+        return df
+
+class Size(Calculation):
+    """
+    计算个股市值相关因子的类，包括对数市值（LNCAP）和非线性市值因子（NLSIZE）。
+    """
+
+    def LNCAP(self, df, raw = False):
+        """
+        计算对数市值因子（LNCAP），并根据需要进行预处理。
+
+        :param df: 包含股票市值数据的 DataFrame，需包含 'S_VAL_MV' 列。
+                    - 'S_VAL_MV': 股票市值 例: 10000000 (市值单位需为浮点数)
+        :param raw: 是否返回未经预处理的原始数据，默认为 False。
+        :return: 包含 'LNCAP' 因子的 DataFrame。
+        """
+
+        if df['S_VAL_MV'].dtype != np.float64:
+            df['S_VAL_MV'] = df['S_VAL_MV'].astype(np.float64)
+
+        df['LNCAP'] = np.log(df['S_VAL_MV'])
+
+        if not raw:
+            df = self._preprocess(df, factor_column = 'LNCAP')
+
+        return df
+
+    def NLSIZE(self, df, raw = False):
+        """
+        计算非线性市值因子（NLSIZE），通过回归对数市值的三次方与常数项，并返回残差作为因子。
+
+        :param df: 包含股票市值数据的 DataFrame，需包含 'S_VAL_MV' 列。
+        :param raw: 是否返回未经预处理的原始数据，默认为 False。
+        :return: 包含 'NLSIZE' 因子的 DataFrame。
+        """
+        df = self.LNCAP(df)
+        df['SIZE_CUBED'] = np.power(df['LNCAP'], 3)
+        df['CONSTANT'] = 1
+
+        df = df.dropna(how='any')
+
+        X = df[['LNCAP','CONSTANT']]
+        y = df['SIZE_CUBED']
+
+        model = sm.OLS(y, X).fit()
+        df['NLSIZE'] = model.resid
+
+        if not raw:
+            df = self._preprocess(df, factor_column = 'NLSIZE')
+
+        df.drop(columns=['SIZE_CUBED', 'LNCAP', 'CONSTANT'], inplace=True)
+
+        return df
+
+
+class EarningsYield(GetData, Calculation):
+    """
+    计算收益率因子类，继承自 GetData 类，用于获取所需的基础数据。
+    """
+
+    def __init__(self, df):
+        """
+        初始化 EarningsYield 类实例，准备数据以供后续计算。
+
+        :param df: 包含股票交易数据的 DataFrame，需包含以下列：
+                    - 'S_INFO_WINDCODE': 个股代码 例: '000001.SZ'
+                    - 'TRADE_DT': 交易日期 例: '2022-08-31'
+                    - 其他相关列，如 'S_VAL_MV'
+        """
+        self.prepared_df = self._prepare_data(df)
+
+    def _prepare_data(self, df):
+        """
+        准备和处理数据，合并估值和收益数据，为计算收益率因子做准备。
+
+        :param df: 包含股票交易数据的 DataFrame，需包含以下列：
+                    - 'S_INFO_WINDCODE': 个股代码 例: '000001.SZ'
+                    - 'TRADE_DT': 交易日期 例: '2022-08-31'
+                    - 其他相关列，如 'S_VAL_MV'、'EST_PE' 等
+        :return: 处理后的 DataFrame，包含估值和收益数据，准备进行收益率因子的计算。
+        """
+
+        epibs_df = GetData.est_consensus_rolling_FY1()
+        epibs_df = epibs_df[~epibs_df['S_INFO_WINDCODE'].str.endswith('BJ')]
+        epibs_df['EPIBS'] = 1 / epibs_df['EST_PE']
+
+        grouped = epibs_df.groupby('S_INFO_WINDCODE')
+        for stock_code, group in grouped:
+            if group['EPIBS'].isna().all():
+                epibs_df = epibs_df.drop(group.index)
+
+                epibs_fy0_df = GetData.est_consensus_rolling_FY0(stock_code)
+                epibs_fy0_df['EPIBS'] = 1 / epibs_fy0_df['EST_PE']
+
+                epibs_df = pd.concat([epibs_df, epibs_fy0_df], ignore_index=True)
+
+        epibs_df.sort_values(by='EST_DT', inplace=True)
+        epibs_df['EPIBS'] = epibs_df.groupby('S_INFO_WINDCODE')['EPIBS'].fillna(method='ffill')
+        epibs_df['EST_DT'] = pd.to_datetime(epibs_df['EST_DT'])
+        df = pd.merge(
+            df,
+            epibs_df,
+            left_on = ['S_INFO_WINDCODE', 'TRADE_DT'],
+            right_on = ['S_INFO_WINDCODE', 'EST_DT']
+        )
+
+        earnings_df = GetData.ttmhis_all()
+        earnings_df.sort_values(by='ANN_DT', inplace=True)
+        earnings_df['ANN_DT'] = pd.to_datetime(earnings_df['ANN_DT'])
+        earnings_df['REPORT_PERIOD'] = pd.to_datetime(earnings_df['REPORT_PERIOD'])
+        earnings_df = earnings_df[(earnings_df['ANN_DT'] - earnings_df['REPORT_PERIOD']) < pd.Timedelta(days=365)]
+        earnings_df.reset_index(drop=True, inplace=True)
+
+        df = df.sort_values(by='TRADE_DT')
+        df = pd.merge_asof(
+            df,
+            earnings_df,
+            by='S_INFO_WINDCODE',
+            left_on='TRADE_DT',
+            right_on='ANN_DT',
+            direction='backward'
+        )
+
+        df['EARNINGS_TTM'] = df.groupby('S_INFO_WINDCODE')['EARNINGS_TTM'].fillna(method='ffill')
+        df['CASH_EARNINGS_TTM'] = df.groupby('S_INFO_WINDCODE')['CASH_EARNINGS_TTM'].fillna(method='ffill')
+
+        return df
+
+    def EARNYILD(self, raw: bool = False):
+        """
+        计算收益率因子（EARNYILD），基于EARNINGS_TTM、CASH_EARNINGS_TTM和EPIBS的加权组合。
+
+        :param raw: 是否返回未经预处理的原始数据，默认为 False，即返回预处理后的数据。
+        :return: 返回包含新增列 'EARNYILD' 的 DataFrame。
+        """
+        df = self.prepared_df
+        df['ETOP'] = df['EARNINGS_TTM'] / df['S_VAL_MV']
+        df['CETOP'] = df['CASH_EARNINGS_TTM'] / df['S_VAL_MV']
+
+        for columns in ['EPIBS', 'ETOP', 'CETOP']:
+             df[columns] = df[columns].astype('float64')
+
+        df['EARNYILD'] = 0.68 * df['EPIBS'] + 0.11 * df['ETOP'] + 0.21 * df[
+            'CETOP']
+
+        if not raw:
+            df = self._preprocess(df, factor_column='EARNYILD')
 
         return df
 
