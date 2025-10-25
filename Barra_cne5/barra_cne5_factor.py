e5ccbdd011e15cd981f33234094feef7d5eaad24
CoChristian <1242754584@qq.com>
2025-10-25 21:16:12 +0800

    homelapv2

--- Barra_cne5/barra_cne5_factor.py
+++ Barra_cne5/barra_cne5_factor.py
@@ -265,7 +265,7 @@ class Beta(Calculation):
         df['ALPHA'] = np.nan
         df['BETA'] = np.nan
         df
-        grouped = df.groupby('stock_code', group_keys=False)
+        grouped = df.groupby('stock_code')
 
         # def cal_WLS_beta_alpha(df):
         #     df
@@ -296,6 +296,122 @@ class Beta(Calculation):
                 df.loc[original_df_index, 'BETA'] = df.loc[original_df_index, 'BETA'].shift(1)
 
         df['SIGMA'] = df['STOCK_RETURN']- df['RF_RETURN'] - (df['ALPHA'] + df['BETA'] * df['MKT_RETURN'])
+
+        return df
+
+
+class Momentum(Calculation):
+    """
+    计算个股动量因子的类，继承自 Calculation 类
+    """
+
+    def RSTR(self, df: pd.DataFrame, raw: bool = False) -> pd.DataFrame:
+        """
+        计算个股的RSTR
+
+        :param df: 包含个股交易数据的 DataFrame，需包含以下列:
+                   - 'TRADE_DT': 交易日期 例: '2022-08-31'
+                   - 'S_INFO_WINDCODE': 个股代码 例: '000001.SZ'
+                   - 'S_DQ_CLOSE': 收盘价 例: 10.5
+                   - 'S_DQ_PRECLOSE': 前一日收盘价 例: 10.0
+        :param raw: 是否返回未经预处理的原始数据，默认为 False，即返回预处理后的数据
+        :return: 返回包含新增列 'RSTR' 的 DataFrame
+        """
+
+        # 检查无风险收益率数据并合并
+        if 'RF_RETURN' not in df.columns:
+            rf_df = GetData.risk_free()
+            df = df.merge(rf_df, on='TRADE_DT', how='left')
+
+        # 计算个股日收益率
+        if 'STOCK_RETURN' not in df.columns:
+            df['STOCK_RETURN'] = df['close'] / df['pre_close'] - 1
+            df['STOCK_RETURN'] = df['STOCK_RETURN'].astype('float')
+
+        # 生成指数加权权重
+        exp_weight = self._exp_weight(window=504, half_life=126)
+
+        # 按个股代码分组
+        grouped = df.groupby('stock_code')
+
+        for stock_code, group in grouped:
+            # 检查数据长度是否足够计算动量因子
+            if len(group) < 504 + 21:
+                print(f'Not enough data to calculate Momentum for {stock_code}')
+                df.loc[group.index, 'RSTR'] = np.nan
+
+            else:
+                # 计算超额对数收益率
+                group['EX_LG_RT'] = np.log(1 + group['STOCK_RETURN']) - np.log(1 + group['RF_RETURN'])
+
+                try:
+                    # 使用指数加权和滚动窗口计算 RSTR
+                    group['RSTR'] = group['EX_LG_RT'].shift(21).rolling(window=504).apply(
+                        lambda x: np.sum(x * exp_weight))
+
+                    # 将计算结果存入原始数据框
+                    df.loc[group.index, 'RSTR'] = group['RSTR']
+
+                except Exception as e:
+                    print(f'Error processing {stock_code}: {e}')
+                    df.loc[group.index, 'RSTR'] = np.nan
+
+        # 如果 raw 为 False，则进行预处理
+        if not raw:
+            df = self._preprocess(df, factor_column='RSTR')
+
+        return df
+
+
+class Size(Calculation):
+    """
+    计算个股市值相关因子的类，包括对数市值（LNCAP）和非线性市值因子（NLSIZE）。
+    """
+
+    def LNCAP(self, df, raw = False):
+        """
+        计算对数市值因子（LNCAP），并根据需要进行预处理。
+
+        :param df: 包含股票市值数据的 DataFrame，需包含 'S_VAL_MV' 列。
+                    - 'S_VAL_MV': 股票市值 例: 10000000 (市值单位需为浮点数)
+        :param raw: 是否返回未经预处理的原始数据，默认为 False。
+        :return: 包含 'LNCAP' 因子的 DataFrame。
+        """
+
+        if df['capitalization'].dtype != np.float64:
+            df['capitalization'] = df['capitalization'].astype(np.float64)
+
+        df['LNCAP'] = np.log(df['capitalization'])
+
+        if not raw:
+            df = self._preprocess(df, factor_column = 'LNCAP')
+
+        return df
+
+    def NLSIZE(self, df, raw = False):
+        """
+        计算非线性市值因子（NLSIZE），通过回归对数市值的三次方与常数项，并返回残差作为因子。
+
+        :param df: 包含股票市值数据的 DataFrame，需包含 'S_VAL_MV' 列。
+        :param raw: 是否返回未经预处理的原始数据，默认为 False。
+        :return: 包含 'NLSIZE' 因子的 DataFrame。
+        """
+        df = self.LNCAP(df)
+        df['SIZE_CUBED'] = np.power(df['LNCAP'], 3)
+        df['CONSTANT'] = 1
+
+        df = df.dropna(how='any')
+
+        X = df[['LNCAP','CONSTANT']]
+        y = df['SIZE_CUBED']
+
+        model = sm.OLS(y, X).fit()
+        df['NLSIZE'] = model.resid
+
+        if not raw:
+            df = self._preprocess(df, factor_column = 'NLSIZE')
+
+        df.drop(columns=['SIZE_CUBED', 'LNCAP', 'CONSTANT'], inplace=True)
 
         return df
 
@@ -336,6 +452,10 @@ class Beta(Calculation):
 
 if __name__ == '__main__':
     total_data = GetData._get_price_()
+    total_data = total_data[total_data['year']>=2016]
+    valuation_data = GetData._get_valuation()
+    size_calculator = Size()
+
     Beta_data = Beta(total_data)
     beta_df = Beta_data.beta_df
     print("test")
