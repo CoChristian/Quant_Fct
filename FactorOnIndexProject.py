f6e0e6ad18dc1f1728df062ad7f53781e4ef8db0
CoChristian <1242754584@qq.com>
2025-11-09 15:20:00 +0800

    homelapv2

---
+++ FactorOnIndexProject.py
@@ -0,0 +1,51 @@
+# get all total price data
+from Barra_cne5 import barra_cne5_factor
+
+
+GetData.get_price_()
+
+# preprocess(check col name, dropduplicate, sort, shift, )
+#
+#
+#
+#
+#
+# # get trade date
+#
+# GetData._get_index()
+#
+# preprocess(check col name, dropduplicate, sort, shift, )
+#
+# get asked freq trade date (func)
+#
+#
+# # get factor  include barra
+#
+# preprocess factor file
+#
+#
+# # merge trade date
+#
+# main trade_date(freq)
+# merge factor df and barra df get factor explode at specific freq
+#
+# merge total_price df to get ret and forcast ret
+#
+#
+# # single factor test(all func writen)
+# exclude st. paused , limit up and down
+#
+# starting date
+#
+#
+# # index enhance
+#
+# get index relation file
+#
+# merge trade date
+# merge index data to get index ret
+# merge total data and get each date ret and stock ret corellation (specific freq)
+
+
+
+
