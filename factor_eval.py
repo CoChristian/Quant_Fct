58407243f1900c2704d7d3c55c5d9286b2a0e00d
CoChristian <1242754584@qq.com>
2026-03-25 19:07:39 +0800

    20260325

--- factor_eval.py
+++ factor_eval.py
@@ -19,3 +19,33 @@ def get_factor_table(start_date, cur_trade_date):
     # last_date = factor_table['trade_date'].values.max()
     # last_date_valuation_table = factor_table[factor_table['trade_date'] == last_date]
     return factor_table
+
+def get_price_table(start_date, end_date):
+    price_table_info = {
+        "engine": "mysql+pymysql://root:swisschina@192.168.110.5:3306/factor_research_full_a_share",
+        "table": 'daily_0935am_trade_price',
+        "field": ['trade_date','code','close'],
+        "index": ['trade_date', "code"],
+        "name_dict": {'close':'close0935'}
+    }
+    price_table = get_data_from_multi_source([price_table_info], start_date, end_date)
+    price_table.reset_index(inplace=True)
+    # last_date = factor_table['trade_date'].values.max()
+    # last_date_valuation_table = factor_table[factor_table['trade_date'] == last_date]
+    return price_table
+
+def get_opt2trade_table(start_date, end_date):
+    date_tale_info = {
+        "engine": "mysql+pymysql://root:swisschina@192.168.110.5:3306/factor_compute_new",
+        "table": 'opt2trade',
+        "field": [],
+        "index": ['opt_date'],
+        "name_dict": {'trade_date': 'trade_date_price'}
+    }
+    date_table = get_data_from_multi_source([date_tale_info], start_date, end_date)
+    date_table.reset_index(inplace=True)
+    date_table['trade_date_price'] = date_table['trade_date_price'].astype('int')
+    return date_table
+
+
+
